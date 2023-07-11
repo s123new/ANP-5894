3a53cf0db63263112ddc07198d2036c48ae2484b
s123new <137862350+s123new@users.noreply.github.com>
2023-07-11 13:07:16 +0530

    Add files via upload

---
+++ Hands-on/Integer.java
@@ -0,0 +1,26 @@
+package loops;
+
+import java.util.*;
+
+class Integer {
+
+	public static void main(String[] args) {
+		
+		Scanner i = new Scanner(System.in);
+		
+			System.out.println(" Enter a number : ");
+		
+			int a = i.nextInt();
+			
+			if(a>0)
+			{
+				System.out.println(" The number is positive.");
+			}
+			else
+			{
+				System.out.println("The number is negetive.");
+			}
+
+	}
+
+}
